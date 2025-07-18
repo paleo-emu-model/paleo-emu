73bcc89982a325efb84beb663747746ad8443f30
Xinrenn <xinxin.ren@bristol.ac.uk>
2025-07-18 14:36:16 +0100

    fix file path

--- examples/emulation_pipeline.py
+++ examples/emulation_pipeline.py
@@ -1,5 +1,6 @@
 
 # from src.optimise import full_emulator_experiment
+import os
 from paleo_emu.training_leave1out import run_training
 
 emulator = "lowmod_ice"
@@ -15,22 +16,24 @@ vae_config = {
     "kl_weight": 0.1 # 0.1, 0.5, 1.0
 }
 
+file_path = os.path.join(".", "examples", "training_data")
+
 # config_dict.py
 train_dict = {
     "lowmod_ice": {
-        "file_path": "/Users/bo20541/Library/CloudStorage/OneDrive-UniversityofBristol/TONIC-Oligocene/paleo-emu/examples/training_data/",
+        "file_path": file_path,
         "X_input": "training_data_lowmodice_temp_formatted.res",
         "Y_output": "training_data_lowmodice_temp_formatted.nc",
         "label": "lowmod_ice"
     },
     "highmod_ice": {
-        "file_path": "/Users/bo20541/Library/CloudStorage/OneDrive-UniversityofBristol/TONIC-Oligocene/paleo-emu/examples/training_data/",
+        "file_path": file_path,
         "X_input": "training_data_highmodice_temp_formatted.res",
         "Y_output": "training_data_highmodice_temp_formatted.nc",
         "label": "highmod_ice"
     },
     "highlowmod_ice": {
-        "file_path": "/Users/bo20541/Library/CloudStorage/OneDrive-UniversityofBristol/TONIC-Oligocene/paleo-emu/examples/training_data/",
+        "file_path": file_path,
         "X_input": "training_data_highlowmodice_temp_formatted.res",
         "Y_output": "training_data_highlowmodice_temp_formatted.nc",
         "label": "highlowmod_ice"
