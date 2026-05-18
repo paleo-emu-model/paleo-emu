a93d60266f1dc16a3c15dbfa0a8bb87d0593864a
XinRenn <xinxin.ren@bristol.ac.uk>
2026-05-18 18:26:52 +0100

    remove unwanted test print

--- paleo_emu/validation.py
+++ paleo_emu/validation.py
@@ -22,11 +22,6 @@ def compute_r2_map(Y_true_out, Y_pred_out,lat_array, lon_array):
             y_pred_series = Y_pred_out[:, i, j]
             if np.all(np.isfinite(y_true_series)) and np.all(np.isfinite(y_pred_series)):
                 if np.std(y_true_series) > 1e-6:
-                    r2 = r2_score(y_true_series, y_pred_series)
-                    r2_map[i, j] = r2
-                else:
-                    print(f"[DEBUG] Skipping lat index {i}, lon index {j} due to low std deviation in y_true_series")
-            else:
-                print(f"[DEBUG] Skipping lat index {i}, lon index {j} due to non-finite values")
+                    r2_map[i, j] = r2_score(y_true_series, y_pred_series)
 
     return r2_map
