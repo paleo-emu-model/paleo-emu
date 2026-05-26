fix the vae loss computation

paleo_emu/encoders.py
@@ -68,7 +68,7 @@ class _VAE(keras.Model):
 def _compute_vae_loss(x, x_decoded, mean, logvar):
     reconstruction_loss = tf.reduce_mean(tf.square(x - x_decoded))
     kl_loss = -0.5 * tf.reduce_mean(1 + logvar - tf.square(mean) - tf.exp(logvar))
-    return reconstruction_loss + kl_loss
+    return reconstruction_loss, kl_loss
 
 def _save_vae_log(epoch_losses, latent_dim, epochs, learning_rate, batch_size, kl_weight, log_dir="training/logs"):
 
@@ -208,9 +208,10 @@ class EncoderGenerator:
             for step, x_batch in enumerate(dataset):
                 with tf.GradientTape() as tape:
                     x_decoded, mean, logvar = vae_model(x_batch)
-                    loss = _compute_vae_loss(
+                    recon_loss, kl_loss = _compute_vae_loss(
                         x_batch, x_decoded, mean, logvar
-                    ) * kl_weight
+                    )
+                    loss = recon_loss + kl_weight * kl_loss
 
                 grads = tape.gradient(loss, vae_model.trainable_variables)
                 grads, _ = tf.clip_by_global_norm(grads, 1.0)