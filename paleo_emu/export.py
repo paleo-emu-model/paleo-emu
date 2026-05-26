39d2a429489f5599eeffae3033c4c79648a510ff
XinRenn <xinxin.ren@bristol.ac.uk>
2026-05-26 17:30:58 +0100

    remove training log saving

--- paleo_emu/export.py
+++ paleo_emu/export.py
@@ -6,9 +6,7 @@ save the training log for VAE.
 
 import re
 import xarray as xr
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import yaml
 from pathlib import Path
 import os
@@ -69,50 +67,6 @@ try:
 except PackageNotFoundError:
     _VERSION = _PKG_META.get("version", "unknown")
 
-
-
-def save_training_log(epoch_losses, latent_dim, epochs, learning_rate, batch_size, kl_weight, log_dir="training/logs"):
-
-    os.makedirs(log_dir, exist_ok=True)
-
-    info_str = f"latent{latent_dim}_ep{epochs}_lr{learning_rate}_bs{batch_size}_kl{kl_weight}"
-
-    loss_curve_filename = os.path.join(log_dir, f"loss_curve_{info_str}.png")
-
-    plt.figure(figsize=(8,5))
-    plt.plot(range(1, len(epoch_losses)+1), epoch_losses, label="Training Loss")
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
-    plt.title(f"VAE Loss Curve ({info_str})")
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-    plt.savefig(loss_curve_filename, dpi=300)
-    plt.close()
-
-    print(f"[INFO] Loss curve saved to: {loss_curve_filename}")
-
-    # --- save hyperparameters and final loss to CSV ---
-    log_file = os.path.join(log_dir, "vae_hyperparameter_log.csv")
-
-    log_entry = {
-        "latent_dim": latent_dim,
-        "epochs": epochs,
-        "learning_rate": learning_rate,
-        "batch_size": batch_size,
-        "kl_weight": kl_weight,
-        "final_loss": epoch_losses[-1]  # the loss of the final epoch
-    }
-
-    if not os.path.exists(log_file):
-        df = pd.DataFrame([log_entry])
-        df.to_csv(log_file, index=False)
-    else:
-        df = pd.read_csv(log_file)
-        df = pd.concat([df, pd.DataFrame([log_entry])], ignore_index=True)
-        df.to_csv(log_file, index=False)
-
-    print(f"[INFO] Hyperparameter log updated: {log_file}")
 
 
 def save_prediction(Y_pred, Y_var, lat_array, lon_array, output_dir, file_name="prediction",
